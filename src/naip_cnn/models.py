9f38f09e516ea1ae616f266bf93cb036b7282bff
Aaron Zuspan <aa.zuspan@gmail.com>
2023-09-18 19:39:29 +0000

    Add basic UNet model

--- src/naip_cnn/models.py
+++ src/naip_cnn/models.py
@@ -39,3 +39,24 @@ def CNN_v1(
     model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
 
     return model
+
+
+def UNet(
+    learn_rate=0.001, shape: tuple[int, int, int] = (150, 150, 4), out_shape=(5, 5)
+):
+    model = tf.keras.models.Sequential(
+        [
+            layers.Conv2D(32, (3, 3), activation="relu", input_shape=shape),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+            layers.Dense(units=out_shape[0] * out_shape[1], activation="linear"),
+            layers.Reshape(out_shape),
+        ]
+    )
+
+    optimizer = Adam(learning_rate=learn_rate)
+    model.compile(loss="mean_squared_error", optimizer=optimizer)
+
+    return model
