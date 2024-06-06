9b22056487cb0ba47daa8fe1bf9bea16d686bc2f
Aaron Zuspan <aa.zuspan@gmail.com>
2024-06-06 21:21:43 +0000

    Record n samples

--- src/naip_cnn/utils.py
+++ src/naip_cnn/utils.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import h5py
+import numpy as np
+
 if TYPE_CHECKING:
     from naip_cnn.data import NAIPDatasetWrapper
     from naip_cnn.models import ModelRun
@@ -30,6 +33,37 @@ def str_to_float(s: str) -> float:
     return float(s.replace("p", "."))
 
 
+def count_duplicate_images(train_path, val_path, key: str = "image") -> int:
+    """
+    Count the number of duplicate images between a train and validation HDF dataset.
+
+    This is useful for ensuring that data leakage didn't occur during data splitting.
+
+    References
+    ----------
+    https://stackoverflow.com/questions/8317022/get-intersecting-rows-across-two-2d-numpy-arrays
+    """
+    with h5py.File(val_path, "r") as f:
+        val_image = f[key][:]
+    with h5py.File(train_path, "r") as f:
+        train_image = f[key][:]
+
+    # Flatten the arrays
+    val_image = val_image.reshape(val_image.shape[0], -1)
+    train_image = train_image.reshape(train_image.shape[0], -1)
+
+    val_dtype = {
+        "names": [f"f{i}" for i in range(val_image.shape[1])],
+        "formats": val_image.shape[1] * [val_image.dtype],
+    }
+    train_dtype = {
+        "names": [f"f{i}" for i in range(train_image.shape[1])],
+        "formats": train_image.shape[1] * [train_image.dtype],
+    }
+
+    return np.intersect1d(val_image.view(val_dtype), train_image.view(train_dtype))
+
+
 def build_wandb_config(
     *,
     dataset: NAIPDatasetWrapper,
@@ -39,6 +73,8 @@ def build_wandb_config(
     batch_size: int,
     learn_rate: float,
     epochs: int,
+    n_train: int,
+    n_val: int,
 ) -> dict:
     """Build a configuration dictionary for tracking an experiment with W&B."""
     return {
@@ -53,6 +89,10 @@ def build_wandb_config(
         },
         "data": {
             "path": dataset._train.path.as_posix(),
+            "n_samples": {
+                "train": n_train,
+                "val": n_val,
+            },
             "date": {
                 "start": dataset.acquisition.start_date,
                 "end": dataset.acquisition.end_date,
